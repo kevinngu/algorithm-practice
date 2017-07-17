718b759e53e23432f47c63fd567b16bc682020a8
kevinngu <jz_kevin_yu@163.com>
2017-07-17 23:28:54 +0800

    add comparable interface.

--- src/sorting/SelectionSort.java
+++ src/sorting/SelectionSort.java
@@ -1,18 +1,35 @@
 public class SelectionSort{
 
     public static void main(String args[]){
-        int arrs[] = {10,9,8,7,6,5,4,3,2,1};
-        selectionSort(arrs,10);
+        Integer arrs[] = {10,9,8,7,6,5,4,3,2,1};
+        selectionSort(arrs,arrs.length);
         for(int i=0; i<arrs.length; i++){
-            System.out.print(arrs[i]);
+            System.out.print(arrs[i] + " ");
+        }
+
+        System.out.println("");
+
+        Double arrs2[] = {10.9,9.9,8.8,7.7,8.6};
+        selectionSort(arrs2,arrs2.length);
+        for(int i=0; i<arrs2.length; i++){
+            System.out.print(arrs2[i] + " ");
+        }
+
+        System.out.println("");
+
+        String arrs3[] = {"B","C","D","A"};
+        selectionSort(arrs3,arrs3.length);
+        for(int i=0; i<arrs3.length; i++){
+            System.out.print(arrs3[i] + " ");
         }
     }
 
-    private static void selectionSort(int arr[], int n){
+    private static void selectionSort(Comparable arr[], int n){
         for(int i = 0; i < n; i++){
+            //find min value in [i, n)
             int minIndex = i;
             for(int j = i+1; j<n; j++){
-                if(arr[j] < arr[minIndex]){
+                if(arr[j].compareTo(arr[minIndex]) < 0){
                     minIndex = j;
                 }
             }
@@ -20,8 +37,8 @@ public class SelectionSort{
         }
     }
 
-    private static void swap(int arr[], int currIndex, int minIndex){
-        int temp = arr[minIndex];
+    private static void swap(Object arr[], int currIndex, int minIndex){
+        Object temp = arr[minIndex];
         arr[minIndex] = arr[currIndex];
         arr[currIndex] = temp;
     }
